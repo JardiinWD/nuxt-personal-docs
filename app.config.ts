c84dbe4321084953d3c553c3dc240db8765b88cd
Alessandro Pecorilla <APecorillaLearning@Yahoo.com>
2023-09-22 19:35:23 +0200

    Scritta sezione del Protected Route e aggiunto nuovo contenuto a react router

--- app.config.ts
+++ app.config.ts
@@ -1,27 +1,11 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docs',
+    title: 'Documentazione',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-    socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
-    },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
-    },
     aside: {
       level: 0,
-      collapsed: false,
+      collapsed: true,
       exclude: []
     },
     main: {
@@ -29,8 +13,8 @@ export default defineAppConfig({
       fluid: true
     },
     header: {
-      logo: true,
-      showLinkIcon: true,
+      logo: false,
+      showLinkIcon: false,
       exclude: [],
       fluid: true
     }
